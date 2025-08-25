7088e82c985022868c58f91dd283c390b5d74cb9
salamafazlul <105430673+salamafazlul@users.noreply.github.com>
2025-08-25 16:22:37 +0530

    Add GET endpoints for questions and choices

--- main.py
+++ main.py
@@ -26,6 +26,20 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/questions/{question_id}")
+async def read_question(question_id: int, db:db_dependency):
+    result = db.query(models.Questions).filter(models.Questions.id == question_id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail='Question is not found')
+    return result
+
+@app.get("/choices/{question_id}")
+async def read_choices(question_id: int, db:db_dependency):
+    result = db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
+    if not result:
+        raise HTTPException(status_code=404, detail='Choices is not found')
+    return result
     
 @app.post("/questions/")
 async def create_questions(question: QuestionBase, db: db_dependency):
